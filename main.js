a9308faf1bc80725809197c275b0c18f72827159
kyeoungchan <wkc1002@naver.com>
2023-01-04 21:53:16 +0900

    Add files via upload
    Completed function of Updating files and pages

--- main.js
+++ main.js
@@ -103,7 +103,7 @@ const app = http.createServer(function(request,response){
         const title = 'WEB - Create';
         const list = templeList(data);
         const template = templateHTML(title, list, `
-          <form action="http://localhost:3000/create_process" method="post">
+          <form action="/create_process" method="post">
             <!-- 입력한 정보를 action값의 서버 주소로 전송하고 싶다는 의미 -->
             <!-- 서버에서 전송받은 정보를 활용하려면 각 정보에 name 속성값이 필요하다. -->
             <p><input type="text" name="title" placeholder="title"></p>
@@ -122,7 +122,7 @@ const app = http.createServer(function(request,response){
         response.end(template);
       });
     });
-  } else if(pathname=='/create_process') {
+  } else if(pathname == '/create_process') {
     let body = '';
     request.on('data', function(data) {
       // Web Broser가 Post 방식으로 Data를 처리할 때, 한 번에 너무 방대한 양의 Data를 처리하면 프로그램이 꺼지거나 컴퓨터에 무리갈 수 있다.
@@ -143,6 +143,58 @@ const app = http.createServer(function(request,response){
         response.end();
         // Page에 띄울 것이 없으므로 인자는 생략한다.
       });
+    });
+  } else if(pathname == '/update') {
+    // pathname은 '?' 기호가 나오기 전으로 끊긴다.
+    fs.readdir('./data', (err, data) => {
+      fs.readFile(`data/${queryData.id}`, `utf8`, (err, description) => {
+        // data 폴더에 있는 title의 명을 가진 파일을 utf8 형식으로 뽑아
+        // if (err) throw err;
+        const title = queryData.id;
+        const list = templeList(data);
+        const template = templateHTML(title, list,
+          `
+          <form action="/update_process" method="post">
+            <!-- 입력한 정보를 action값의 서버 주소로 전송하고 싶다는 의미 -->
+            <!-- 서버에서 전송받은 정보를 활용하려면 각 정보에 name 속성값이 필요하다. -->
+            <input type="hidden" name="id" value="${title}">
+            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+            <p>
+              <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <!-- Text를 여러 줄 입력할 수 있는 입력창이다. -->
+            <p>
+              <input type="submit">
+              <!-- 전송 버튼이 생겨난다. -->
+            </p>
+          </form>
+          `,
+          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+        );
+        response.writeHead(200);
+        // 파일이 성공적으로 전송 했다고 Web Server가 Web Browser에게 알려주는 약속된 언어
+        response.end(template);
+      });
+    });
+  } else if(pathname == '/update_process') {
+    let body = '';
+    request.on('data', function(data) {
+      body += data;
+    });
+    request.on('end', function() {
+      const post = qs.parse(body);
+      const id = post.id;
+      const title = post.title;
+      const description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, (err) => {
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+          // CallBack 함수이므로 file이 rename이 된 후에 writeFile() 함수가 실행된다.
+          // 그러므로 변경해야 하는 파일명은 id가 아닌 title이다.
+          response.writeHead(302, {location : `/?id=${title}`});
+          response.end();
+        });
+      });
+      console.log(post);
     });
   } else {
     // 그 외의 경로로 접속했다면
