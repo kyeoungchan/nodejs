10a69dc936728af75aee441240aa5ca08192dea4
kyeoungchan <wkc1002@naver.com>
2023-01-08 15:01:46 +0900

    Add files via upload
    refactored codes using 'template' object

--- main.js
+++ main.js
@@ -4,51 +4,51 @@ const url = require('url');
 // 우리는 'url'이라는 모듈을 url이라는 변수를 통해서 사용할 것이라고 nodejs에게 알려주는 것이다.
 const qs = require('querystring');
 
-function templateHTML(title, list, body, control) {
-  // HTML 코드 사용의 반복을 줄이기 위한 함수
-  // template HTML 코드 내용들을 data 인자를 활용할 수 있게끔 이 안으로 옮겨준다.
-  return `
-  <!doctype html>
-  <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-      <link rel="stylesheet" href="style.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-      <script src="colors.js"></script>
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <div id="grid">
-        <div id="aList">
-          <!-- 버튼과 목록을 하나의 aList라는 id의 값으로 묶어준다. -->
-          <input type="button" value="night" onclick="
-            nightDayHandler(this);
-          ">
-          ${list}
-          ${control}
+const template = {
+  html:function(title, list, body, control) {
+    // HTML 코드 사용의 반복을 줄이기 위한 함수
+    // template HTML 코드 내용들을 data 인자를 활용할 수 있게끔 이 안으로 옮겨준다.
+    return `
+    <!doctype html>
+    <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+        <script src="colors.js"></script>
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <div id="grid">
+          <div id="aList">
+            <!-- 버튼과 목록을 하나의 aList라는 id의 값으로 묶어준다. -->
+            <input type="button" value="night" onclick="
+              nightDayHandler(this);
+            ">
+            ${list}
+            ${control}
+          </div>
+          <div id="article">
+            ${body}
+          </div>
         </div>
-        <div id="article">
-          ${body}
-        </div>
-      </div>
-    </body>
-  </html>
-  `;
-}
-
-function templeList(data) {
-  // 파일 list를 출력하기 위한 함수
-  let list = '<ul>';
-  // HTML 코드를 기계적으로 생성하고 담기 위한 list 변수 선언
-  i = 0;
-  while(i < data.length) {
-    // 파일 갯수만큼 자동적으로 <li> Tag 형식의 HTML 코드를 생성해주는 반복문
-    list = list + `<li><a href="/?id=${data[i]}">${data[i]}</a></li>`;
-    i+=1;
+      </body>
+    </html>
+    `;
+  }, list:function(data) {
+    // 파일 list를 출력하기 위한 함수
+    let list = '<ul>';
+    // HTML 코드를 기계적으로 생성하고 담기 위한 list 변수 선언
+    i = 0;
+    while(i < data.length) {
+      // 파일 갯수만큼 자동적으로 <li> Tag 형식의 HTML 코드를 생성해주는 반복문
+      list = list + `<li><a href="/?id=${data[i]}">${data[i]}</a></li>`;
+      i+=1;
+    }
+    list = list + '</ul>';
+    return list;
   }
-  list = list + '</ul>';
-  return list;
 }
 
 const app = http.createServer(function(request,response){
@@ -68,14 +68,14 @@ const app = http.createServer(function(request,response){
           // data 폴더에 있는 'Web'이라는 Data 파일을 연다.
           // if (err) throw err;
           const title = 'Welcome';
-          const list = templeList(data);
-          const template = templateHTML(title, list,
+          const list = template.list(data);
+          const html = template.html(title, list,
             `<h2>${title}</h2><p>${description}</p>`,
             `<a href="/create">create</a>`
           );
           response.writeHead(200);
           // 파일이 성공적으로 전송 했다고 Web Server가 Web Browser에게 알려주는 약속된 언어
-          response.end(template);
+          response.end(html);
         });
       });
     } else {
@@ -84,8 +84,8 @@ const app = http.createServer(function(request,response){
           // data 폴더에 있는 title의 명을 가진 파일을 utf8 형식으로 뽑아
           // if (err) throw err;
           const title = queryData.id;
-          const list = templeList(data);
-          const template = templateHTML(title, list,
+          const list = template.list(data);
+          const html = template.html(title, list,
             `<h2>${title}이란</h2><p>${description}</p>`,
             `
             <a href="/create">create</a>
@@ -98,7 +98,7 @@ const app = http.createServer(function(request,response){
           );
           response.writeHead(200);
           // 파일이 성공적으로 전송 했다고 Web Server가 Web Browser에게 알려주는 약속된 언어
-          response.end(template);
+          response.end(html);
         });
       });
     }
@@ -108,8 +108,8 @@ const app = http.createServer(function(request,response){
         // data 폴더에 있는 'Web'이라는 Data 파일을 연다.
         // if (err) throw err;
         const title = 'WEB - Create';
-        const list = templeList(data);
-        const template = templateHTML(title, list, `
+        const list = template.list(data);
+        const html = template.html(title, list, `
           <form action="/create_process" method="post">
             <!-- 입력한 정보를 action값의 서버 주소로 전송하고 싶다는 의미 -->
             <!-- 서버에서 전송받은 정보를 활용하려면 각 정보에 name 속성값이 필요하다. -->
@@ -126,7 +126,7 @@ const app = http.createServer(function(request,response){
         `, '');
         response.writeHead(200);
         // 파일이 성공적으로 전송 했다고 Web Server가 Web Browser에게 알려주는 약속된 언어
-        response.end(template);
+        response.end(html);
       });
     });
   } else if(pathname == '/create_process') {
@@ -158,8 +158,8 @@ const app = http.createServer(function(request,response){
         // data 폴더에 있는 title의 명을 가진 파일을 utf8 형식으로 뽑아
         // if (err) throw err;
         const title = queryData.id;
-        const list = templeList(data);
-        const template = templateHTML(title, list,
+        const list = template.list(data);
+        const html = template.html(title, list,
           `
           <form action="/update_process" method="post">
             <!-- 입력한 정보를 action값의 서버 주소로 전송하고 싶다는 의미 -->
@@ -180,7 +180,7 @@ const app = http.createServer(function(request,response){
         );
         response.writeHead(200);
         // 파일이 성공적으로 전송 했다고 Web Server가 Web Browser에게 알려주는 약속된 언어
-        response.end(template);
+        response.end(html);
       });
     });
   } else if(pathname == '/update_process') {
